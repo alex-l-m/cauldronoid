350ebd9751d31374940c5437d762cd8424c7507a
Alexander Davis <alexlovesmolecules@gmail.com>
2022-05-03 19:57:51 -0400

    Onehot encoding function now returns a pandas dataframe rather than numpy array

--- rdkit2pandas.py
+++ rdkit2pandas.py
@@ -1,10 +1,12 @@
 import numpy as np
+from pandas import DataFrame
 
 def encode_factor(observations, levels):
     '''Given length n list of observations (strings) and length k list of
-    levels (strings), output n by k matrix (numpy array) of one-hot
-    encodings'''
+    levels (strings), output n by k matrix (pandas dataframe, with columns
+    labeled by levels) of one-hot encodings'''
     factor_dict = dict((symbol, i) for i, symbol in enumerate(levels))
     factor_indices = [factor_dict[obs] for obs in observations]
     factor_onehot = np.identity(len(levels))[factor_indices,:]
-    return factor_onehot
+    factor_df = DataFrame(factor_onehot, columns = levels, dtype = int)
+    return factor_df
