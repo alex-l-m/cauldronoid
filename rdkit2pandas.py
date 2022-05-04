cf32c94a9eec5714d8f1170cf38078130b98bef2
Alexander Davis <alexlovesmolecules@gmail.com>
2022-05-03 21:31:01 -0400

    Added function to extract a numeric property from a molecule to pandas

--- rdkit2pandas.py
+++ rdkit2pandas.py
@@ -1,5 +1,7 @@
+'''Functions to construct Pandas dataframes from RDKit molecules'''
+
 import numpy as np
-from pandas import DataFrame
+from pandas import DataFrame, Series
 
 def encode_factor(observations, levels):
     '''Given length n list of observations (strings) and length k list of
@@ -10,3 +12,9 @@ def encode_factor(observations, levels):
     factor_onehot = np.identity(len(levels))[factor_indices,:]
     factor_df = DataFrame(factor_onehot, columns = levels, dtype = int)
     return factor_df
+
+def extract_double(key, objs):
+    '''Given an iterable sequence "obs" of atoms, bonds, or molecules (anything
+    with a GetProp method), return the property specified by "key" as a Pandas
+    series, with the name specified by "key". For a numeric property'''
+    return Series([obj.GetDoubleProp(key) for obj in objs], name = key, dtype = np.float32)
