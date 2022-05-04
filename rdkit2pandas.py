df918e644294888704836583b132a329fb5e05e2
Alexander Davis <alexlovesmolecules@gmail.com>
2022-05-03 21:46:32 -0400

    Modified factor function to take in atoms molecules or bonds and a key, instead of values

--- rdkit2pandas.py
+++ rdkit2pandas.py
@@ -3,18 +3,20 @@
 import numpy as np
 from pandas import DataFrame, Series
 
-def encode_factor(observations, levels):
-    '''Given length n list of observations (strings) and length k list of
-    levels (strings), output n by k matrix (pandas dataframe, with columns
-    labeled by levels) of one-hot encodings'''
-    factor_dict = dict((symbol, i) for i, symbol in enumerate(levels))
-    factor_indices = [factor_dict[obs] for obs in observations]
-    factor_onehot = np.identity(len(levels))[factor_indices,:]
-    factor_df = DataFrame(factor_onehot, columns = levels, dtype = int)
-    return factor_df
-
 def extract_double(key, objs):
     '''Given an iterable sequence "obs" of atoms, bonds, or molecules (anything
     with a GetProp method), return the property specified by "key" as a Pandas
     series, with the name specified by "key". For a numeric property'''
     return Series([obj.GetDoubleProp(key) for obj in objs], name = key, dtype = np.float32)
+
+def extract_factor(levels, key, objs):
+    '''Given length n list of observations (strings) and length k list of
+    objects with a GetProp method, output n by k matrix (pandas dataframe, with
+    columns labeled by levels) of one-hot encodings of the property named "key"
+    (a string)'''
+    observations = [obj.GetProp(key) for obj in objs]
+    factor_dict = dict((symbol, i) for i, symbol in enumerate(levels))
+    factor_indices = [factor_dict[obs] for obs in observations]
+    factor_onehot = np.identity(len(levels))[factor_indices,:]
+    factor_df = DataFrame(factor_onehot, columns = levels, dtype = int)
+    return factor_df
